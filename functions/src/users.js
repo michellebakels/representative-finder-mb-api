6b66b7396ee3e8b86175b3ed41be32721663ca5e
michellebakels <michelle.bakels@gmail.com>
2021-07-28 16:45:53 -0400

    get user

---
+++ functions/src/users.js
@@ -0,0 +1,41 @@
+const admin = require('firebase-admin')
+const credentials = require('../credentials.json')
+
+function connectDb() {
+    if (!admin.apps.length) {
+        admin.initializeApp({
+            credential: admin.credential.cert(credentials)
+        })
+    }
+    return admin.firestore()
+}
+
+exports.getUser = (req, res) => {
+    const db = connectDb()
+
+    db.collection('users')
+        .where('email', '==', req.params.email)
+        .get()
+        .then((snapshot) => {
+            snapshot.forEach((doc) => {
+                const user = doc.data()
+                user.id = doc.id
+                res.status(200).json({
+                    status: 'success',
+                    data: user,
+                    message: 'User loaded successfully',
+                    statusCode: 200,
+                })
+            })
+        })
+        .catch(err => res.status(500).send('User could not be found'))
+}
+
+exports.createUser = (req, res) => {
+    const db = connectDb()
+
+    db.collection('users')
+        .add(req.body)
+        .then(docRef => res.send({id: docRef.id}))
+        .catch(err => res.status(500).send('User could not be created'))
+}
